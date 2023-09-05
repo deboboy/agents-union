368637264bfed2752abcdc4d9254df3fef7b7630
frank martinez <frank@lastmyle.co>
2023-09-04 22:26:42 -0700

    changed landing page image reference

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
           <Image
             className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/a96f3f84-b4f4-49b5-9ad0-184c687ac9be.jpeg"
+            src="/hiring-hall.jpeg"
             alt="labor union hiring hall in the style of Seattle World's Fair, 1962"
             width={512}
             height={512}
