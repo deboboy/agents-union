4cfcaa3197a67e6fdb573ad6b1977c63f013fb98
frank martinez <frank@lastmyle.co>
2023-09-06 20:32:56 -0700

    add array logic, store generated images; prompt variants

--- app/page.js
+++ app/page.js
@@ -17,17 +17,17 @@ export default function Home() {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between p-4">
-        <div className="z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm lg:flex">
+      <main className="flex min-h-screen flex-col items-center p-2">
+        <div className="mb-4 z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm lg:flex">
           <h1 className="text-4xl font-bold leading-tight tracking-tight text-center lg:text-left">
             Decentralized Agents Organization
           </h1>
         </div>
 
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className="relative flex place-items-center">
           <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/hiring-hall.jpeg"
+            className="relative"
+            src="/ea8ed497-90ce-4921-b2d7-350c0d9bbc4b.jpeg"
             alt="labor union hiring hall in the style of Seattle World's Fair, 1962"
             width={512}
             height={512}
@@ -35,22 +35,24 @@ export default function Home() {
           />
         </div>
 
-        <div className="mb-4 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left flex items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <Link href="/gallery">
-              <h2 className="mb-3 text-2xl font-semibold">
-                Agents{' '}
-                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                  -&gt;
-                </span>
-              </h2>
-              <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                Go here to hire union agents.
-              </p>
-            </Link>
-          </div>
+        <div className="mt-4 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left flex items-center justify-center">
+          <Link href="/gallery">
+            <button 
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded" 
+            >
+              Enter Studio
+            </button>
+          </Link>
+        </div>
+        <div className="mt-4">
+          <p className="text-xs text-center">
+            © 2023 @deboboy
+          </p>
         </div>
       </main>
+      {/* <footer className="w-full text-center text-xs p-2 pin-b">
+        <p>© 2023 @deboboy</p>
+      </footer> */}
     </>
   )
 }
--- components/hugging-face.js
+++ components/hugging-face.js
@@ -1,12 +1,28 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const HFGenerate = () => {
-  const [imageUrl, setImageUrl] = useState('');
-  const [inputText, setInputText] = useState('');
+  const [imageUrls, setImageUrls] = useState(JSON.parse(localStorage.getItem('imageUrls')) || []);
+
+  useEffect(() => {
+    // Update the imageUrls state when the component mounts
+    const storedImageUrls = localStorage.getItem('imageUrls');
+    setImageUrls(JSON.parse(storedImageUrls) || []);
+  }, []);
 
   const handleClick = async () => {
+    // Array of possible inputs
+    const inputsArray = [
+      "flat vector illustration, png white background, clean, minimalistic:1.5, headshot of a robot facing the camera with a smile on it's face, in the style of animated illustrations, studyplace, text-based",
+      "3D vector illustration, png green background, busy, headshot of a robot facing the camera with a smile on it's face, in the style of game charactere",
+      "headshot of a robot with a frown on it's face",
+      // Add as many as you want
+    ];
+
+    // Select a random input
+    const randomInput = inputsArray[Math.floor(Math.random() * inputsArray.length)];
+
     try {
       const response = await fetch('https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5', {
         method: 'POST',
@@ -15,7 +31,7 @@ const HFGenerate = () => {
           'Content-Type': 'application/json'
         },
         // body: JSON.stringify({ inputs: inputText })
-        body: JSON.stringify({ inputs: "an artificial intelligence agents, in the style of a Mexican punk robot, 1960s" })
+        body: JSON.stringify({ inputs: randomInput })
       });
   
       if (!response.ok) {
@@ -29,7 +45,11 @@ const HFGenerate = () => {
   
       const blob = await response.blob();
       const url = URL.createObjectURL(blob);
-      setImageUrl(url);
+      const newImageUrls = [...imageUrls, url];
+      setImageUrls(newImageUrls);
+      if (typeof window !== 'undefined') {
+        localStorage.setItem('imageUrls', JSON.stringify(newImageUrls));
+      }
     } catch (error) {
       console.log(error);
     }
@@ -37,24 +57,17 @@ const HFGenerate = () => {
 
   return (
     <div className="flex flex-col justify-left p-4">
-      {/* <input 
-        type="text" 
-        value={inputText} 
-        onChange={(e) => setInputText(e.target.value)} 
-        className="shadow appearance-none border rounded w-3/4 py-4 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-2 mr-2"
-        placeholder="Describe your agent..."
-      /> */}
       <button 
         className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded " 
         onClick={handleClick}
       >
-        Recruit
+        Generate Agent
       </button>
-      {imageUrl && 
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 pt-2">
-          <img className="w-full h-auto border-4 border-white" src={imageUrl} alt="Generated" />
+      {imageUrls.map((url, index) => (
+        <div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-full pt-2">
+          <img className="w-full h-auto border-4 border-white" src={url} alt={`Generated ${index}`} />
         </div>
-      }
+      ))}
     </div>
   );
 };
