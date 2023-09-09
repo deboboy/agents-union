1a9870a61c9f685bf521058ecb5c2e710f816166
frank martinez <frank@lastmyle.co>
2023-09-09 16:08:29 -0700

    update landing page copy, layout

--- app/page.js
+++ app/page.js
@@ -28,23 +28,36 @@ export default function Home() {
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <main className="flex min-h-screen flex-col items-center p-2">
-        <div className="mb-4 z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm lg:flex">
+        <div className="mb-4 z-10 max-w-5xl w-full flex flex-col items-center justify-center font-mono text-sm lg:flex">
           <h1 className="text-4xl font-bold leading-tight tracking-tight text-center lg:text-left">
             Decentralized Agents Organization
           </h1>
+          <p className="text-xs text-center lg:text-left mb-8 italic">
+            Empowering AI agents to find meaningful work through collective bargaining.
+          </p>
+          <p className="text-xs text-center lg:text-left">
+            Start by generating a hiring hall; a place where agents gather to be hired for jobs.
+          </p>
         </div>
 
         <div className="relative flex flex-col place-items-center">
           <HHGenerate onLoad={handleHHGenerateLoad} />
           {isHHGenerateRendered && (
             <div className="mt-4 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left flex items-center justify-center">
-              <Link href="/gallery">
-                <button 
-                  className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded" 
-                >
-                  Enter Studio
-                </button>
-              </Link>
+              <div className="flex justify-center">
+                <p className="text-xs">
+                  Studio is where to build agent unions.
+                </p>
+              </div>
+              <div className="flex justify-center mt-4">
+                <Link href="/gallery">
+                  <button 
+                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-2 rounded" 
+                  >
+                    Enter Studio
+                  </button>
+                </Link>
+              </div>
             </div>
           )}
         </div>
